0ae0de949bc60777e52d453561ccde94d1aae6df
tommy <tommy2888takashi@yahoo.co.jp>
2020-07-27 00:50:21 +0000

    kadai 001

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
      if logged_in?
      @task = current_user.tasks.build
      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+     end
   end
 
   def show
